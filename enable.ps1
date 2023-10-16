60ac35d8b9e61b4f8f3d608a938ec357c565c0f6
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2023-10-16 16:28:21 +0200

    Create enable.ps1

---
+++ enable.ps1
@@ -0,0 +1,146 @@
+########################################
+# HelloID-Conn-Prov-Target-YsisV2-Enable
+#
+# Version: 1.0.0
+########################################
+# Initialize default values
+$config = $configuration | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
+$aRef = $AccountReference | ConvertFrom-Json
+$success = $false
+$auditLogs = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+# Enable TLS1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+# Set debug logging
+switch ($($config.IsDebug)) {
+    $true { $VerbosePreference = 'Continue' }
+    $false { $VerbosePreference = 'SilentlyContinue' }
+}
+
+#region functions
+function Resolve-YsisV2Error {
+    param (
+        [object]
+        $ErrorObject
+    )
+    $httpErrorObj = [PSCustomObject]@{
+        ScriptLineNumber = $ErrorObject.InvocationInfo.ScriptLineNumber
+        Line             = $ErrorObject.InvocationInfo.Line
+        ErrorDetails     = $ErrorObject.Exception.Message
+        FriendlyMessage  = $ErrorObject.Exception.Message
+    }
+
+    try {
+        if ($null -eq $ErrorObject.ErrorDetails){
+            $streamReaderResponse = [System.IO.StreamReader]::new($ErrorObject.Exception.Response.GetResponseStream()).ReadToEnd()
+            if($null -ne $streamReaderResponse){
+                $convertedError = $streamReaderResponse | ConvertFrom-Json
+                $httpErrorObj.ErrorDetails = "Message: $($convertedError.error), description: $($convertedError.error_description)"
+                $httpErrorObj.FriendlyMessage =  "Message: $($convertedError.error), description: $($convertedError.error_description)"
+            }
+        } else {
+            $errorResponse = $ErrorObject.ErrorDetails | ConvertFrom-Json
+            $httpErrorObj.ErrorDetails = "Message: $($errorResponse.detail), type: $($errorResponse.scimType)"
+            $httpErrorObj.FriendlyMessage = "$($errorResponse.detail), type: $($errorResponse.scimType)"
+        }
+    } catch {
+        $httpErrorObj.FriendlyMessage = "Received an unexpected response. The JSON could not be converted, error: [$($_.Exception.Message)]. Original error from web service: [$($ErrorObject.Exception.Message)]"
+    }
+    Write-Output $httpErrorObj
+}
+#endregion
+
+# Begin
+try {
+    # Verify if [aRef] has a value
+    if ([string]::IsNullOrEmpty($($aRef))) {
+        throw 'The account reference could not be found'
+    }
+
+    # Requesting authorization token
+    $splatRequestToken = @{
+        Uri    = "$($config.BaseUrl)/cas/oauth/token"
+        Method = 'POST'
+        Body   = @{
+            client_id     = $($config.ClientID)
+            client_secret = $($config.ClientSecret)
+            scope         = 'scim'
+            grant_type    = 'client_credentials'
+        }
+    }
+    $responseAccessToken = Invoke-RestMethod @splatRequestToken -Verbose:$false
+
+    Write-Verbose 'Adding Authorization headers'
+    $headers = [System.Collections.Generic.Dictionary[string, string]]::new()
+    $headers.Add('Authorization', "Bearer $($responseAccessToken.access_token)")
+    $headers.Add('Accept', 'application/json')
+    $headers.Add('Content-Type', 'application/json')
+
+    Write-Verbose "Verifying if YsisV2 account for [$($p.DisplayName)] exists"
+    try {
+        $splatParams = @{
+            Uri         = "$($config.BaseUrl)/gm/api/um/scim/v2/users/$($aRef.Id)"
+            Headers     = $headers
+            ContentType = 'application/json'
+        }
+        $responseUser = Invoke-RestMethod @splatParams -Verbose:$false
+    }
+    catch {
+        if ($_.Exception.Response.StatusCode -eq 404) {
+            $auditLogs.Add([PSCustomObject]@{
+                    Message = "YsisV2 account for: [$($p.DisplayName)] not found. Possibly deleted"
+                    IsError = $false
+                })
+        }
+        throw
+    }
+
+    # Add an auditMessage showing what will happen during enforcement
+    if ($dryRun -eq $true) {
+        Write-Warning "[DryRun] Enable YsisV2 account for: [$($p.DisplayName)] will be executed during enforcement"
+    }
+
+    # Process
+    if (-not($dryRun -eq $true)) {
+        Write-Verbose "Enabling YsisV2 account with accountReference: [$($aRef.Id)]"
+        $responseUser.active = $true
+        $splatParams = @{
+            Uri         = "$($config.BaseUrl)/gm/api/um/scim/v2/users/$($aRef.Id)"
+            Headers     = $headers
+            Method      = 'PUT'
+            Body        = $responseUser | ConvertTo-Json
+            ContentType = 'application/scim+json'
+        }
+        $null = Invoke-RestMethod @splatParams -Verbose:$false
+
+        $success = $true
+        $auditLogs.Add([PSCustomObject]@{
+                Message = 'Enable account was successful'
+                IsError = $false
+            })
+    }
+} catch {
+    $success = $false
+    $ex = $PSItem
+    if ($($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObj = Resolve-YsisV2Error -ErrorObject $ex
+        $auditMessage = "Could not enable YsisV2 account. Error: $($errorObj.FriendlyMessage)"
+        Write-Verbose "Error at Line '$($errorObj.ScriptLineNumber)': $($errorObj.Line). Error: $($errorObj.ErrorDetails)"
+    } else {
+        $auditMessage = "Could not enable YsisV2 account. Error: $($ex.Exception.Message)"
+        Write-Verbose "Error at Line '$($ex.InvocationInfo.ScriptLineNumber)': $($ex.InvocationInfo.Line). Error: $($ex.Exception.Message)"
+    }
+    $auditLogs.Add([PSCustomObject]@{
+            Message = $auditMessage
+            IsError = $true
+        })
+# End
+} finally {
+    $result = [PSCustomObject]@{
+        Success   = $success
+        Auditlogs = $auditLogs
+    }
+    Write-Output $result | ConvertTo-Json -Depth 10
+}
