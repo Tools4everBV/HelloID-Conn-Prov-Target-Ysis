81446c25b56b8d793e56db4a3a33c9d487ccbe37
r.jongbloed@tools4ever.com <r.jongbloed@tools4ever.com>
2024-07-23 11:47:23 +0200

    Minor improvements in logging of correlation

--- create.ps1
+++ create.ps1
@@ -276,7 +276,7 @@ try {
 
             $outputContext.AuditLogs.Add([PSCustomObject]@{
                     Action  = "CorrelateAccount"
-                    Message = "Account with Ysis Initials [$($account.ysisInitials)] and username [$($account.UserName)] correlated on field [$($correlationField)] with value [$($correlationValue)]"
+                    Message = "Account with Ysis Initials [$($ysisAccount.ysisInitials)] and username [$($ysisAccount.UserName)] correlated on field [$($correlationField)] with value [$($correlationValue)]"
                     IsError = $false
                 })
 
